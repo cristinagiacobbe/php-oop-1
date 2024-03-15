9271c665a61e7e2e4005354d8c2baf398260e46f
cristinagiacobbe <crigiak@gmail.com>
2024-03-15 23:55:37 +0100

    Create static property and static function

--- index.php
+++ index.php
@@ -15,16 +15,34 @@ Facciamo attenzione all’organizzazione del codice, suddividendolo in appositi 
 Possiamo ad esempio organizzare il codice
 - creando un file dedicato ai dati (tipo le array di oggetti) che potremmo chiamare db.php
 - mettendo ciascuna classe nel proprio file e magari raggruppare tutte le classi in una cartella dedicata che possiamo chiamare Models/
-- organizzando il layout dividendo la struttura ed i contenuti in file e parziali dedicati. */
+- organizzando il layout dividendo la struttura ed i contenuti in file e parziali dedicati. 
+
+Aggiungiamo nella classe Movie:
+- una proprietá statica che richiamiamo staticamente
+- un metodo 'normale' nel quale usiamo la proprietá statica
+- un metodo statico che chiamiamo staticamente
+
+
+*/
 
 class Movie
 {
+    public static string $nationality = "USA";
 
-    public function __construct(public string $title, public int $year, public string $nationality, public string $genre, public $age)
+    public function Nationality()
+    {
+        return self::$nationality;
+    }
+
+    public static function StaticNationality()
+    {
+        return "USA";
+    }
+
+    public function __construct(public string $title, public int $year, public string $genre, public $age)
     {
         $this->title = $title;
         $this->year = $year;
-        $this->nationality = $nationality;
         $this->genre = $genre;
         $this->age = $age = "";
     }
@@ -39,10 +57,10 @@ class Movie
 }
 
 
-$MovieA = new Movie('Inside out', 2020, 'USA', 'cartoon', '');
-$MovieB = new Movie('La vita à bella', 2000, 'Italy', 'comedy', '');
-$MovieC = new Movie("C'è ancora domani", 2023, 'Italy', 'comedy', '');
-$MovieD = new Movie('Via col vento', 1970, 'USA', 'drama', '');
+$MovieA = new Movie('Inside out', 2015, 'cartoon', '');
+$MovieB = new Movie('Mary Poppins', 1964,  'comedy', '');
+$MovieC = new Movie("Oppenheimer", 2023, 'comedy', '');
+$MovieD = new Movie('Via col vento', 1939, 'drama', '');
 
 $ageMovieA = $MovieA->IsModern($MovieA->year);
 $ageMovieB = $MovieB->IsModern($MovieB->year);
@@ -58,3 +76,34 @@ var_dump($MovieC);
 var_dump($ageMovieC);
 var_dump($MovieD);
 var_dump($ageMovieD);
+var_dump(Movie::$nationality)
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oop Movies</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Four film made in <?php echo $MovieA->Nationality() ?></h1>
+        <h1>Four film made in <?php echo Movie::StaticNationality()   ?></h1>
+        <div class="row">
+            <div class="col">
+                <?php foreach ($Movie as $EachMovie) : ?>
+                    <div class="card">
+                        <?php echo $EachMovie->title ?>
+                        <?php echo $EachMovie->year ?>
+                        <?php echo $EachMovie->genre ?>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
